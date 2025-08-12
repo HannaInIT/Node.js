d233ffcee43a316f365a4d7792aa9dba21a1c8ba
Hanna Dubyna <ann10062016@gmail.com>
2025-08-12 14:09:22 +0200

    Completed practice exercises week1 node

--- week1/practice-exercises/2-left-pad/script.js
+++ week1/practice-exercises/2-left-pad/script.js
@@ -1,3 +1,5 @@
+// const getLeft = require("./padLeft");
+const leftPad = require('left-pad')
 /**
  ** Exercise 2: To the left, to the left...
  * 
@@ -11,3 +13,6 @@
 let numbers = [ "12", "846", "2", "1236" ];
 
 // YOUR CODE GOES HERE
+for (let i = 0; i < numbers.length; i++) {
+  console.log(leftPad(numbers[i], 8, " "));
+}
